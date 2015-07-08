e034dad0910df7f0b861f2bd69976d3a73a21f60
Calum Lacroix <calum.lacroix@macmillan.com>
2015-07-08 15:56:15 +0100

    Add more environment variables for use in Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,19 @@ FROM maven:3.2-jdk-7-onbuild
 VOLUME /usr/src/app/etc
 WORKDIR /usr/src/app
 
-
 # Defaults that work with 
 # https://registry.hub.docker.com/u/durdn/atlassian-crowd/dockerfile/
 ENV CROWD_APP_NAME crowd-ldap-server
 ENV CROWD_APP_PW crowd-ldap-server
 ENV CROWD_APP_URL http://crowd:8095/
 ENV CROWD_URL http://crowd:8095/crowd
-
-EXPOSE 10389
+ENV SSL_ENABLE false
+ENV SSL_KEYSTORE /opt/crowd-ldap-server.keystore
+ENV SSL_PASSWORD changeit
+ENV PORT 10389
+ENV AD_MEMBEROF false
+ENV AD_INCLUDE_NESTED false
+ENV SSL_DEBUG false
+EXPOSE $PORT
 
 CMD ["/usr/src/app/run.sh"]
--- run.sh
+++ run.sh
@@ -4,10 +4,6 @@ JAR=target/crowd-ldap-server-*.jar
 # Apache DS Settings
 FIXADS="-Duser.language=en -Duser.country=US"
 
-# SSL Debugging
-#DEBUG_SSL="-Djavax.net.debug=ssl"
-DEBUG_SSL=
-
 # Override etc/crowd.properties, e.g. from Docker
 if [ -n "${CROWD_APP_NAME+1}" ]; then
   CROWD="-Dapplication.name=$CROWD_APP_NAME $CROWD" 
@@ -21,8 +17,28 @@ fi
 if [ -n "${CROWD_URL+1}" ]; then
   CROWD="-Dcrowd.server.url=$CROWD_URL $CROWD" 
 fi
-
+if [ -n "${AD_MEMBEROF+1}" ]; then
+  AD="-Demulate.ad.memberof=$AD_MEMBEROF $AD"
+fi
+if [ -n "${AD_INCLUDE_NESTED+1}" ]; then
+  AD="-Demulate.ad.include.nested=$AD_INCLUDE_NESTED $AD"
+fi
+if [ -n "${LISTENER_PORT+1}" ]; then
+  OPTS="-Dlistener.port=$LISTENER_PORT $OPTS"
+fi
+if [ -n "${SSL_ENABLE+1}" ]; then
+  SSL="-Dssl.enabled=$SSL_ENABLE $SSL"
+fi
+if [ -n "${SSL_KEYSTORE+1}" ]; then
+  SSL="-Dssl.keystore=$SSL_KEYSTORE $SSL"
+fi
+if [ -n "${SSL_PASSWORD+1}" ]; then
+  SSL="-Dssl.certificate.password=$SSL_PASSWORD $SSL"
+fi
+if [[ "${SSL_DEBUG}" == "true" ]]; then
+  SSL="-Djavax.net.debug=ssl $SSL"
+fi
 
 # Run Server
-exec java $CROWD $FIXADS $DEBUG_SSL -cp etc -jar $JAR $*
+exec java $OPTS $AD $CROWD $FIXADS $SSL -cp etc -jar $JAR $*
 
